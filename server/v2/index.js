48947481e1cf4ef0a1cf2f4b1fd7ff5f5c848600
raphael_rpi <raphaelbridi@yahoo.com>
2019-05-09 14:02:14 +0000

    another server fully working like v1

--- server/v2/index.js
+++ server/v2/index.js
@@ -1,1 +1,38 @@
-console.log("hello world from the rpi!");
+const app = require('express')();
+const ReceiveData = require('./services/ReceiveData')
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+const server = app.listen(3000);
+const io = require('./socket').init(server);
+const port = require('./serialPort').init('/dev/ttyACM0', 9600);
+
+//----------------------- SOCKET LISTENERS --------------------------
+
+let clients = 0;
+io.on('connection', socket => {
+  //Tell us if a client connected
+  clients++;
+  console.log(`CONNECT: ${clients} client(s) connected!`);
+
+  //Here are the messages received from the web
+  socket.on("myClientMessage", (data) => {
+    ReceiveData.sendBack(data); //send back to the web
+    port.write(data); //send to the arduino
+  })
+
+  //Tell us if a client disconnected
+  socket.on("disconnect", () => {
+    clients--;
+    console.log(`DISCONNECT: ${clients} client(s) connected!`);
+  });
+});
+
