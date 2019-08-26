818c7725bafef69744c5cef7aceecbc1aa8af6d0
Raphael <raphaelbridi@yahoo.com>
2019-08-25 23:11:41 -0400

    added keyboard

---
+++ web/v2/src/constants/keyboard.js
@@ -0,0 +1,213 @@
+import AImg from "../assets/keyboard/computer_key_A.png";
+import BImg from "../assets/keyboard/computer_key_B.png";
+import CImg from "../assets/keyboard/computer_key_C.png";
+import DImg from "../assets/keyboard/computer_key_D.png";
+import EImg from "../assets/keyboard/computer_key_E.png";
+import FImg from "../assets/keyboard/computer_key_F.png";
+import GImg from "../assets/keyboard/computer_key_G.png";
+import HImg from "../assets/keyboard/computer_key_H.png";
+import IImg from "../assets/keyboard/computer_key_I.png";
+import JImg from "../assets/keyboard/computer_key_J.png";
+import KImg from "../assets/keyboard/computer_key_K.png";
+import LImg from "../assets/keyboard/computer_key_L.png";
+import MImg from "../assets/keyboard/computer_key_M.png";
+import NImg from "../assets/keyboard/computer_key_N.png";
+import OImg from "../assets/keyboard/computer_key_O.png";
+import PImg from "../assets/keyboard/computer_key_P.png";
+import QImg from "../assets/keyboard/computer_key_Q.png";
+import RImg from "../assets/keyboard/computer_key_R.png";
+import SImg from "../assets/keyboard/computer_key_S.png";
+import TImg from "../assets/keyboard/computer_key_T.png";
+import UImg from "../assets/keyboard/computer_key_U.png";
+import VImg from "../assets/keyboard/computer_key_V.png";
+import WImg from "../assets/keyboard/computer_key_W.png";
+import XImg from "../assets/keyboard/computer_key_X.png";
+import YImg from "../assets/keyboard/computer_key_Y.png";
+import ZImg from "../assets/keyboard/computer_key_Z.png";
+
+import UpImg from "../assets/keyboard/computer_key_Arrow_Up.png";
+import DownImg from "../assets/keyboard/computer_key_Arrow_Down.png";
+import RightImg from "../assets/keyboard/computer_key_Arrow_Right.png";
+import LeftImg from "../assets/keyboard/computer_key_Arrow_Left.png";
+
+//---------------------------------------------------------
+
+export const Up = {
+  name: "Up",
+  image: UpImg,
+  mapTo: "1"
+};
+
+export const Down = {
+  name: "Down",
+  image: DownImg,
+  mapTo: "2"
+};
+
+export const Right = {
+  name: "Right",
+  image: RightImg,
+  mapTo: "3"
+};
+
+export const Left = {
+  name: "Left",
+  image: LeftImg,
+  mapTo: "4"
+};
+
+export const A = {
+  name: "A",
+  image: AImg,
+  mapTo: "a"
+};
+
+export const B = {
+  name: "B",
+  image: BImg,
+  mapTo: "b"
+};
+
+export const C = {
+  name: "C",
+  image: CImg,
+  mapTo: "c"
+};
+
+export const D = {
+  name: "D",
+  image: DImg,
+  mapTo: "d"
+};
+
+export const E = {
+  name: "E",
+  image: EImg,
+  mapTo: "e"
+};
+
+export const F = {
+  name: "F",
+  image: FImg,
+  mapTo: "f"
+};
+
+export const G = {
+  name: "G",
+  image: GImg,
+  mapTo: "g"
+};
+
+export const H = {
+  name: "H",
+  image: HImg,
+  mapTo: "h"
+};
+
+export const I = {
+  name: "I",
+  image: IImg,
+  mapTo: "i"
+};
+
+export const J = {
+  name: "J",
+  image: JImg,
+  mapTo: "j"
+};
+
+export const K = {
+  name: "K",
+  image: KImg,
+  mapTo: "k"
+};
+
+export const L = {
+  name: "L",
+  image: LImg,
+  mapTo: "l"
+};
+
+export const M = {
+  name: "M",
+  image: MImg,
+  mapTo: "m"
+};
+
+export const N = {
+  name: "N",
+  image: NImg,
+  mapTo: "n"
+};
+
+export const O = {
+  name: "O",
+  image: OImg,
+  mapTo: "o"
+};
+
+export const P = {
+  name: "P",
+  image: PImg,
+  mapTo: "p"
+};
+
+export const Q = {
+  name: "Q",
+  image: QImg,
+  mapTo: "q"
+};
+
+export const R = {
+  name: "R",
+  image: RImg,
+  mapTo: "r"
+};
+
+export const S = {
+  name: "S",
+  image: SImg,
+  mapTo: "s"
+};
+
+export const T = {
+  name: "T",
+  image: TImg,
+  mapTo: "t"
+};
+
+export const U = {
+  name: "U",
+  image: UImg,
+  mapTo: "u"
+};
+
+export const V = {
+  name: "V",
+  image: VImg,
+  mapTo: "v"
+};
+
+export const W = {
+  name: "W",
+  image: WImg,
+  mapTo: "w"
+};
+
+export const X = {
+  name: "X",
+  image: XImg,
+  mapTo: "x"
+};
+
+export const Y = {
+  name: "Y",
+  image: YImg,
+  mapTo: "y"
+};
+
+export const Z = {
+  name: "Z",
+  image: ZImg,
+  mapTo: "z"
+};
