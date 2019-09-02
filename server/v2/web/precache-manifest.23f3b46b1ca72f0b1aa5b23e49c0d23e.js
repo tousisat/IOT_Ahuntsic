f61c1a0a7152c9784d6fcd1922dd7cf1e58fa1e0
Raphael <raphaelbridi@yahoo.com>
2019-09-02 14:03:36 -0400

    added website into rpi server

---
+++ server/v2/web/precache-manifest.23f3b46b1ca72f0b1aa5b23e49c0d23e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c500529c425c37e27678a3682da399a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ab52a06295568663d21",
+    "url": "/static/css/main.37d58059.chunk.css"
+  },
+  {
+    "revision": "50540956c4f890f47a6d",
+    "url": "/static/js/2.a8fe179c.chunk.js"
+  },
+  {
+    "revision": "3ab52a06295568663d21",
+    "url": "/static/js/main.2aeb3c9e.chunk.js"
+  },
+  {
+    "revision": "963e368e2aa162bebce2",
+    "url": "/static/js/runtime~main.06df887e.js"
+  },
+  {
+    "revision": "29ac4edec5136925c58a68afe7a747df",
+    "url": "/static/media/noCamera.29ac4ede.svg"
+  },
+  {
+    "revision": "88a3fcb607405612f122ed346fb449f2",
+    "url": "/static/media/robot.88a3fcb6.svg"
+  }
+]);
